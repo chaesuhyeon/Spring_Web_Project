e708d8f6b7adc20a70ff7e81070461085fd58d5d
suhyeon <ddd8177@gmail.com>
2022-10-18 12:48:33 +0900

    커스텀 UserDetailsService 사용하여 로그인, remember-me

--- jex06/src/main/java/org/zerock/config/SecurityConfig.java
+++ jex06/src/main/java/org/zerock/config/SecurityConfig.java
@@ -1,37 +1,67 @@
 package org.zerock.config;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.zerock.security.CustomLoginSuccessHandler;
+import org.zerock.security.CustomUserDetailsService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity // 스프링 MVC와 스프링 시큐리티를 결합하는 용도로 사용
 @Log4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter { // security-context.xml 대신 하는 설정
 
-	@Override
+	@Setter(onMethod_ = @Autowired)
+	private DataSource dataSource;
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		log.info("configure................................");
+//
+//		auth.inMemoryAuthentication()
+//				.withUser("admin")
+//				.password("{noop}admin")
+//				.roles("ADMIN");
+//
+//		auth.inMemoryAuthentication()
+//				.withUser("member")
+//				.password("$2a$10$hVk6v0/RQ70PEvYBfmpZLeMyPCPwMG5AZVoU28y7xG7T/kXfMC50m")
+//				.roles("MEMBER");
+////		auth.inMemoryAuthentication().withUser("member").password("{noop}member").roles("MEMBER");
+//
+//	}
+
+//	@Override // JDBC를 이용하는 Java 설정
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		log.info("configure JDBC ..............................");
+//
+//		String queryUser = "select userid, userpw, enabled from tbl_member where userid = ? ";
+//		String queryDetails = "select userid, auth from tbl_member_auth where userid = ?";
+//
+//		auth.jdbcAuthentication()
+//				.dataSource(dataSource)
+//				.passwordEncoder(passwordEncoder())
+//				.usersByUsernameQuery(queryUser)
+//				.authoritiesByUsernameQuery(queryDetails);
+//	}
+
+	@Override // Custom UserDetailsService
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		log.info("configure................................");
-
-		auth.inMemoryAuthentication()
-				.withUser("admin")
-				.password("{noop}admin")
-				.roles("ADMIN");
-
-		auth.inMemoryAuthentication()
-				.withUser("member")
-				.password("$2a$10$hVk6v0/RQ70PEvYBfmpZLeMyPCPwMG5AZVoU28y7xG7T/kXfMC50m")
-				.roles("MEMBER");
-//		auth.inMemoryAuthentication().withUser("member").password("{noop}member").roles("MEMBER");
-
+		auth.userDetailsService(customUserService()).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
@@ -40,8 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // security-c
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
+	public UserDetailsService customUserService(){
+		return new CustomUserDetailsService();
 	}
 
 	@Override // <security:http> 관련 설정들을 대신
@@ -60,7 +90,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // security-c
 				.logoutUrl("/customLogout")
 				.invalidateHttpSession(true)
 				.deleteCookies("remember-me", "JSESSION_ID");
+
+		http.rememberMe()
+				.key("zerock")
+				.tokenRepository(persistentTokenRepository()).tokenValiditySeconds(604800);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean // remember-me 기능 구현시 필요
+	public PersistentTokenRepository persistentTokenRepository(){
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		return repo;
+	}
 
 }
